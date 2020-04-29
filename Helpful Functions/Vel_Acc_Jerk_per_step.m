cf9e4b36a19b98e2740ae5cc3eb3b88248d1551a
mfturra <60787493+mfturra@users.noreply.github.com>
2020-04-29 15:59:49 -0400

    Minor formatting changes

--- Helpful Functions/Vel_Acc_Jerk_per_step.m
+++ Helpful Functions/Vel_Acc_Jerk_per_step.m
@@ -28,25 +28,19 @@ for ii = allSteps(:,3)
 end
 
 %% Create frames of each step to reference
+%Right foot
 if rStep_id >= 0
     rStep_1(:,1) =  rStep_id(1,1):rStep_id(1,2);
     rStep_2(:,1) =  rStep_id(2,1):rStep_id(2,2);
     rStep_3(:,1) =  rStep_id(3,1):rStep_id(3,2);
     rStep_4(:,1) =  rStep_id(4,1):rStep_id(4,2);
-%     rStep_5(:,1) =  rStep_id(5,1):rStep_id(5,2);
+    rStep_5(:,1) =  rStep_id(5,1):rStep_id(5,2);
+else 
+    
 %     rStep_6(:,1) =  rStep_id(6,1):rStep_id(6,2);
 end
 
-% for ii = allSteps(:,1)
-%     rStep_first(:,1) =  ii(1,1):ii(2,1);
-%     rStep_second(:,1) = ii(3,1):ii(4,1);
-%     rStep_third(:,1) =  ii(5,1):ii(6,1);
-% end
-
-% rTO_HS_frames_firstStep = rTO_HS(1):rTO_HS(2);
-% rTO_HS_frames_secondStep = rTO_HS(3):rTO_HS(4);
-% rTO_HS_frames_thirdStep = rTO_HS(5):rTO_HS(6);
-
+%Left foot
 if lStep_id >= 0
     lStep_1(:,1) =  lStep_id(1,1):lStep_id(1,2);
     lStep_2(:,1) =  lStep_id(2,1):lStep_id(2,2);
@@ -56,90 +50,74 @@ if lStep_id >= 0
 %     lStep_6(:,1) =  lStep_id(6,1):lStep_id(6,2);
 end
 
-lTO_HS_frames_firstStep = lTO_HS(1):lTO_HS(2);
-lTO_HS_frames_secondStep = lTO_HS(3):lTO_HS(4);
-lTO_HS_frames_thirdStep = lTO_HS(5):lTO_HS(6);
-lTO_HS_frames_fourthStep = lTO_HS(7):lTO_HS(8);
-lTO_HS_frames_fifthStep = lTO_HS(9):lTO_HS(10);
-lTO_HS_frames_sixthStep = lTO_HS(11):lTO_HS(12);
-lTO_HS_frames_seventhStep = lTO_HS(13):lTO_HS(14);
-
 %% Velocity per step
 %Right foot
-rVel_step1 =        rAnkVel2D(rTO_HS_frames_firstStep);
+rVel_step1 =        rAnkVel2D(rStep_1);
 rStep_vel.step1 =   sum(rVel_step1);
-rVel_step2 =        rAnkVel2D(rTO_HS_frames_secondStep);
+rVel_step2 =        rAnkVel2D(rStep_2);
 rStep_vel.step2 =   sum(rVel_step2);
-rVel_step3 =        rAnkVel2D(rTO_HS_frames_thirdStep);
+rVel_step3 =        rAnkVel2D(rStep_3);
 rStep_vel.step3 =   sum(rVel_step3);
+rVel_step4 =        rAnkVel2D(rStep_4);
+rStep_vel.step4 =   sum(rVel_step4);
 
 %Left foot
-lVel_step1 =        lAnkVel2D(lTO_HS_frames_firstStep);
+lVel_step1 =        lAnkVel2D(lStep_1);
 lStep_vel.step1 =   sum(lVel_step1);
-lVel_step2 =        lAnkVel2D(lTO_HS_frames_secondStep);
+lVel_step2 =        lAnkVel2D(lStep_2);
 lStep_vel.step2 =   sum(lVel_step2);
-lVel_step3 =        lAnkVel2D(lTO_HS_frames_thirdStep);
+lVel_step3 =        lAnkVel2D(lStep_3);
 lStep_vel.step3 =   sum(lVel_step3);
-lVel_step4 =        lAnkVel2D(lTO_HS_frames_fourthStep);
+lVel_step4 =        lAnkVel2D(lStep_4);
 lStep_vel.step4 =   sum(lVel_step4);
-lVel_step5 =        lAnkVel2D(lTO_HS_frames_fifthStep);
-lStep_vel.step5 =   sum(lVel_step5);
-lVel_step6 =        lAnkVel2D(lTO_HS_frames_sixthStep);
-lStep_vel.step6 =   sum(lVel_step6);
-lVel_step7 =        lAnkVel2D(lTO_HS_frames_seventhStep);
-lStep_vel.step7 =   sum(lVel_step7);
 
 %Array of velocity taking place for right foot
 % rTO_HS_vel = [rAnkVel2D(rTO_HS_frames_firstStep) rAnkVel2D(rTO_HS_frames_secondStep) rAnkVel2D(rTO_HS_frames_thirdStep)];
 
 %% Acceleration per step
 %Right foot
-rAcc_step1 =        rAnkAcc2D(rTO_HS_frames_firstStep);
+rAcc_step1 =        rAnkAcc2D(rStep_1);
 rStep_Acc.step1 =   sum(rAcc_step1);
-rAcc_step2 =        rAnkAcc2D(rTO_HS_frames_secondStep);
+rAcc_step2 =        rAnkAcc2D(rStep_2);
 rStep_Acc.step2 =   sum(rAcc_step2);
-rAcc_step3 =        rAnkAcc2D(rTO_HS_frames_thirdStep);
+rAcc_step3 =        rAnkAcc2D(rStep_3);
 rStep_Acc.step3 =   sum(rAcc_step3);
+rAcc_step4 =        rAnkAcc2D(rStep_4);
+rStep_Acc.step4 =   sum(rAcc_step4);
 
 %Left foot
-lAcc_step1 =        lAnkAcc2D(lTO_HS_frames_firstStep);
+lAcc_step1 =        lAnkAcc2D(lStep_1);
 lStep_Acc.step1 =   sum(lAcc_step1);
-lAcc_step2 =        lAnkAcc2D(lTO_HS_frames_secondStep);
+lAcc_step2 =        lAnkAcc2D(lStep_2);
 lStep_Acc.step2 =   sum(lAcc_step2);
-lAcc_step3 =        lAnkAcc2D(lTO_HS_frames_thirdStep);
+lAcc_step3 =        lAnkAcc2D(lStep_3);
 lStep_Acc.step3 =   sum(lAcc_step3);
-lAcc_step4 =        lAnkAcc2D(lTO_HS_frames_fourthStep);
+lAcc_step4 =        lAnkAcc2D(lStep_4);
 lStep_Acc.step4 =   sum(lAcc_step4);
-lAcc_step5 =        lAnkAcc2D(lTO_HS_frames_fifthStep);
-lStep_Acc.step5 =   sum(lAcc_step5);
-lAcc_step6 =        lAnkAcc2D(lTO_HS_frames_sixthStep);
-lStep_Acc.step6 =   sum(lAcc_step6);
-lAcc_step7 =        lAnkAcc2D(lTO_HS_frames_seventhStep);
-lStep_Acc.step7 =   sum(lAcc_step7);
+% lAcc_step5 =        lAnkAcc2D(lStep_5);
+% lStep_Acc.step5 =   sum(lAcc_step5);
 
 %% Jerk per step
 %Right foot
-rJerk_step1 =        rAnkJerk2D(rTO_HS_frames_firstStep);
+rJerk_step1 =        rAnkJerk2D(rStep_1);
 rStep_Jerk.step1 =   sum(abs(rJerk_step1));
-rJerk_step2 =        rAnkJerk2D(rTO_HS_frames_secondStep);
+rJerk_step2 =        rAnkJerk2D(rStep_2);
 rStep_Jerk.step2 =   sum(abs(rJerk_step2));
-rJerk_step3 =        rAnkJerk2D(rTO_HS_frames_thirdStep);
+rJerk_step3 =        rAnkJerk2D(rStep_3);
 rStep_Jerk.step3 =   sum(abs(rJerk_step3));
+rJerk_step4 =        rAnkJerk2D(rStep_4);
+rStep_Jerk.step5 =   sum(abs(rJerk_step4));
 
 %Left foot
-lJerk_step1 =        lAnkJerk2D(lTO_HS_frames_firstStep);
+lJerk_step1 =        lAnkJerk2D(lStep_1);
 lStep_Jerk.step1 =   sum(abs(lJerk_step1));
-lJerk_step2 =        lAnkJerk2D(lTO_HS_frames_secondStep);
+lJerk_step2 =        lAnkJerk2D(lStep_2);
 lStep_Jerk.step2 =   sum(abs(lJerk_step2));
-lJerk_step3 =        lAnkJerk2D(lTO_HS_frames_thirdStep);
+lJerk_step3 =        lAnkJerk2D(lStep_3);
 lStep_Jerk.step3 =   sum(abs(lJerk_step3));
-lJerk_step4 =        lAnkJerk2D(lTO_HS_frames_fourthStep);
+lJerk_step4 =        lAnkJerk2D(lStep_4);
 lStep_Jerk.step4 =   sum(abs(lJerk_step4));
-lJerk_step5 =        lAnkJerk2D(lTO_HS_frames_fifthStep);
+lJerk_step5 =        lAnkJerk2D(lStep_5);
 lStep_Jerk.step5 =   sum(abs(lJerk_step5));
-lJerk_step6 =        lAnkJerk2D(lTO_HS_frames_sixthStep);
-lStep_Jerk.step6 =   sum(abs(lJerk_step6));
-lJerk_step7 =        lAnkJerk2D(lTO_HS_frames_seventhStep);
-lStep_Jerk.step7 =   sum(abs(lJerk_step7));
 
 
