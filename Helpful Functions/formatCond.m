5bb72cda8766bd4762e3bfb32f2e2413ec8b2547
mfturra <60787493+mfturra@users.noreply.github.com>
2020-05-11 10:12:08 -0400

    formatCond function was cleaned up
    Old code and format was removed from the script of the function

--- Helpful Functions/formatCond.m
+++ Helpful Functions/formatCond.m
@@ -61,30 +61,4 @@ bar(configMatrix_mean)
 hold on
 errorbar(configMatrix_mean,configMatrix_std);
 
-%% First attempt
-
-% condMatrix_test(:,1) = vertcat(condMatrix_unformatted(:,1),condMatrix_unformatted(:,9)); %cond 0
-% condMatrix_test(:,2) = vertcat(condMatrix_unformatted(:,5),condMatrix_unformatted(:,13)); %cond 1
-% condMatrix_test(:,3) = vertcat(condMatrix_unformatted(:,4),condMatrix_unformatted(:,14)); %cond 2
-% condMatrix_test(:,4) = vertcat(condMatrix_unformatted(:,8),condMatrix_unformatted(:,10)); %cond 3
-% condMatrix_test(:,5) = vertcat(condMatrix_unformatted(:,2),condMatrix_unformatted(:,16)); %cond 4
-% condMatrix_test(:,6) = vertcat(condMatrix_unformatted(:,6),condMatrix_unformatted(:,12)); %cond 5
-% condMatrix_test(:,7) = vertcat(condMatrix_unformatted(:,7),condMatrix_unformatted(:,11)); %cond 6
-% condMatrix_test(:,8) = vertcat(condMatrix_unformatted(:,3),condMatrix_unformatted(:,15)); %cond 7
-
 end
-
-
-
-
-%         
-        
-
-
-% condMatrix = reshape(totalCOM_jerk_per_trial,[(length(totalCOM_jerk_per_trial)/12,16]);
-
-% % condMatrix = ones(length(condNum),12);
-% for ii = 1:length(condNum)
-%     condMatrix(1,ii) = totalCOM_jerk_per_trial(ii); %trials 1:16
-%     condMatrix(2,ii) = totalCOM_jerk_per_trial(ii+1:1+length(condNum)); %trials 17:32
-%     condMatrix(3,ii) = totalCOM_jerk_per_trial(ii+1:1+length(condNum)); %trials 33:48
