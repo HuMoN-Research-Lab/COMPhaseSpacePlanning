001219f5e14ae123c1ea7ac92b6249298e0e1da4
mfturra <60787493+mfturra@users.noreply.github.com>
2020-05-12 09:58:44 -0400

    Master script and helper functions were modified
    Master script was cleaned up and organized
    
    Load function was organized and modified to include the filter for the left steps

--- ARSO_MotionCaptureMaster.m
+++ ARSO_MotionCaptureMaster.m
@@ -16,10 +16,10 @@ cd(codePath)
 %create method for changing directory to googledrive
 addpath(genpath(cd)) %%add the current folder & subfolders to the path (so Matlab can see the BTK methods)
 
-%% Trial calc for loop
+%% Experiment Info 
 totalTrials =   192;
-totalCond =     8;
-totalExp =      12;
+totalCond =     8;      %req for formatting trial results
+totalExp =      12;     %req for formatting trial results
 
 for trialNum = 1:totalTrials
     % Identify location where files are stored
@@ -38,70 +38,69 @@ for trialNum = 1:totalTrials
     %% calcBodySegMass function
     % Function outputs mass for individual body segs
     [bodySegMass,segPropWeight] = calcBodySegMass(kgMass);
-
+    
     %% Load data from specific fid
-%     [numFrames,framerate,markerLabels,data_mar_dim_frame,step_TO_HS] ...
-%         = loadPhaseSpaceMoCapData(fid,condTitle);    
+    % Function loads req outputs from .mat files
     [data_mar_dim_frame,markerLabels,numFrames,step_TO_HS,tracker,bb]...
         = loadPhaseSpaceMoCapData(fid);
-%     figure(trialNum)
-%     plot(tracker(1,:))
     
-%    %% Plot MoCap data for trial from start to finish
-% for fr = 805:10:1117
-%     %Clear current frame
-%     clf
-% 
-%     %Plot all markers in x,y, and z
-%     plot3(data_mar_dim_frame(:, 1, fr),...
-%         data_mar_dim_frame(:, 2, fr),...
-%         data_mar_dim_frame(:, 3, fr),'k.','MarkerFaceColor','k')
-%     
-%     hold on
-% %     
-%     %% Uncover the unknown marker location
-%     plot3(tracker(1,fr),...
-%         tracker(2,fr),...
-%         tracker(3,fr),'p','DisplayName','STRN');
-% % 
-% % %     %% Seg Evals in 3D
-% % %     %plot of total anatomical COM
-% % %     plot3(segCenter.RHandCenter_mar_dim_frame(1,fr),...
-% % %         segCenter.RHandCenter_mar_dim_frame(2,fr),...
-% % %         segCenter.RHandCenter_mar_dim_frame(3,fr),'p','DisplayName','Next');
-% %     
-% %     %% Total Body COM in 3D
-% %     %plot of total anatomical COM
-% %     plot3(totalCOMXYZ(1,fr),...
-% %         totalCOMXYZ(2,fr),...
-% %         totalCOMXYZ(3,fr),'p','DisplayName','TotalCOMXYZ');
-% %     
-%     %% Plotting parameters
-%     axis equal
-%     grid on
-%     legend
-%     
-%     %optimal x y z graph limits 
-%     xlim([-1e3 5e3])
-%     ylim([-1e3 1e3]) %full lab
-% %    ylim([1e3 3e3]) %smaller space
-%     zlim([0 3e3])
-%     
-%     %unsure of what this section XYZs
-%     az = -84.362;
-%     el =  20.417;
-%     view(az,el)
-%     
-%     
-%    drawnow
-% end
+    %% Plot full body MoCap for trial from start to finish
+    figure(trialNum)
+    
+    % for fr = 805:10:1117
+    %     %Clear current frame
+    %     clf
+    %
+    %     %Plot all markers in x,y, and z
+    %     plot3(data_mar_dim_frame(:, 1, fr),...
+    %         data_mar_dim_frame(:, 2, fr),...
+    %         data_mar_dim_frame(:, 3, fr),'k.','MarkerFaceColor','k')
+    %
+    %     hold on
+    %
+    %     %% Uncover the unknown marker location
+    %     plot3(tracker(1,fr),...
+    %         tracker(2,fr),...
+    %         tracker(3,fr),'p','DisplayName','STRN');
+    %
+    %     %% Seg Evals in 3D
+    %     %plot of total anatomical COM
+    %     plot3(segCenter.RHandCenter_mar_dim_frame(1,fr),...
+    %         segCenter.RHandCenter_mar_dim_frame(2,fr),...
+    %         segCenter.RHandCenter_mar_dim_frame(3,fr),'p','DisplayName','Next');
+    %
+    %     %% Total Body COM in 3D
+    %     %plot of total anatomical COM
+    %     plot3(totalCOMXYZ(1,fr),...
+    %         totalCOMXYZ(2,fr),...
+    %         totalCOMXYZ(3,fr),'p','DisplayName','TotalCOMXYZ');
+    % %
+    %     %% Plotting parameters
+    %     axis equal
+    %     grid on
+    %     legend
+    %
+    %     %optimal x y z graph limits
+    %     xlim([-1e3 5e3])
+    %     ylim([-1e3 1e3]) %full lab
+    % %    ylim([1e3 3e3]) %smaller space
+    %     zlim([0 3e3])
+    %
+    %     %unsure of what this section XYZs
+    %     az = -84.362;
+    %     el =  20.417;
+    %     view(az,el)
+    %
+    %
+    %    drawnow
+    % end
     
     
-%     %% Butterworth filter
-%     order   = 4;
-%     cutoff  = 7;
-%
-%     [data_mar_dim_frame] = butterLowZero(order,cutoff,framerate,data); % data_mar_dim_frame, %left(1:numel(markerLabels),:,:) right(1:numel(markerLabels),1:3,:) Butterworth filter each marker's data and load it into the trial
+    %     %% Butterworth filter
+    %     order   = 4;
+    %     cutoff  = 7;
+    %
+    %     [data_mar_dim_frame] = butterLowZero(order,cutoff,framerate,data); % data_mar_dim_frame, %left(1:numel(markerLabels),:,:) right(1:numel(markerLabels),1:3,:) Butterworth filter each marker's data and load it into the trial
     
     %% calcSegCOM function
     %Function outputs totalCOM considering marker location
@@ -121,35 +120,25 @@ for trialNum = 1:totalTrials
     totalCOM_jerk_per_frame = totalCOM_calc.marJerk_abs./length(trial_start_end);
     totalCOM_jerk_per_trial(trialNum) = sum(totalCOM_jerk_per_frame);
     
-%     cond = totalCOM_jerk_per_trial
-%     figure(trialNum)
-%     plot(RFoot.marJerk_abs)
-%     hold on
-%     plot(LFoot.marJerk_abs)
+    %     cond = totalCOM_jerk_per_trial
+    %     figure(trialNum)
+    %     plot(RFoot.marJerk_abs)
+    %     hold on
+    %     plot(LFoot.marJerk_abs)
     
-%         %% ZeniStepFinder
-%         % Identify all heel-toe step locations
-%         [allSteps,step_hs_to_ft_XYZ,peaks,hs_to_ft_Data] = ZeniStepFinder_ccpVid_modified(data_mar_dim_frame, markerLabels,framerate);
+    %         %% ZeniStepFinder
+    %         % Identify all heel-toe step locations
+    %         [allSteps,step_hs_to_ft_XYZ,peaks,hs_to_ft_Data] = ZeniStepFinder_ccpVid_modified(data_mar_dim_frame, markerLabels,framerate);
     
-%     %% Vel_Acc_Jerk_per_step
-%     [totalCOM_totalJerk_per_step,totalAnkleJerk_per_step] = Vel_Acc_Jerk_per_step(step_TO_HS,RFoot,LFoot,totalCOM_calc,numFrames);
-% 
-% %     rJerk_per_trial(trialNum) =       rFoot_totalJerk;
-% %     lJerk_per_trial(trialNum) =       lFoot_totalJerk;
-%     totalJerk_per_trial(trialNum) =             totalAnkleJerk_per_step;
-%     totalCOM_totalJerk_per_trial(trialNum) =    totalCOM_totalJerk_per_step;
-        
+    %     %% Vel_Acc_Jerk_per_step
+    %     [totalCOM_totalJerk_per_step,totalAnkleJerk_per_step] = Vel_Acc_Jerk_per_step(step_TO_HS,RFoot,LFoot,totalCOM_calc,numFrames);
+    %
+    % %     rJerk_per_trial(trialNum) =       rFoot_totalJerk;
+    % %     lJerk_per_trial(trialNum) =       lFoot_totalJerk;
+    %     totalJerk_per_trial(trialNum) =             totalAnkleJerk_per_step;
+    %     totalCOM_totalJerk_per_trial(trialNum) =    totalCOM_totalJerk_per_step;
+    
 end
 
 %% Find conditions of different trials
-% [condMatrix] = formatCond(totalCOM_jerk_per_trial,totalCond,totalExp);
-
-for jj = 1:length(rightStep)
-    act_rightStep(1) = rightStep(jj+1);
-end
-    
-%take average across cond
-% errorbar
-% stdev
-% for matching conditions
-
+[condMatrix] = formatCond(totalCOM_jerk_per_trial,totalCond,totalExp);
--- Helpful Functions/loadPhaseSpaceMoCapData.m
+++ Helpful Functions/loadPhaseSpaceMoCapData.m
@@ -3,7 +3,6 @@ function [data_mar_dim_frame,markerLabels,numFrames,step_TO_HS,markerXYZ,x_diff]
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %loadPhaseSpaceMoCapData outputs various variables from the file name 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 %% Load file based on fileName
 % sub01 = [];
 load(fid);
@@ -23,7 +22,6 @@ numFrames = endFrame;  % - startFrames;
 %% Step acquisition
 %perform filter for zenistep finder
 %acquire steps when trial started depending on the STRM movement
-%use those frames to perform first filter to step_TO_HS
 %next filter out all duplicates
 
 %% Track data of unspecified marker for trials
@@ -44,42 +42,28 @@ if ii >= 1.0
     revised_step_TO_HS = step_TO_HS(startStep:end,:);
 end
 
-
 %First step filter
 step_TO_HS = revised_step_TO_HS;
 leftStep = find(step_TO_HS(:,3) == 1);
 rightStep = find(step_TO_HS(:,3) == 2);
+leftStep_filter = diff(leftStep);
 rightStep_filter = diff(rightStep);
 
-rightStep = rightStep(1):rightStep(filter+1);
-
-for jj = rightStep_filter
+for jj = leftStep_filter
     filter = find(jj == 1);
+    leftStep = leftStep(1:filter);
+end
+
+for kk = rightStep_filter
+    filter = find(kk == 1);
     rightStep = rightStep(1:filter);
 end
-    
-%     if jj == rightStep+1
-        
-        act_rightStep = mod(rightStep,2) == 1 %mod step doesnt work, needs to be iter
 
-        
-
-    
-    % detect which step is incorrect and revise it accordingly
-    revStep = mod(leftStep,2);
-    leftStep = leftStep(revStep);
-    
-    revStep = ~mod(rightStep,2);
-    rightStep = rightStep(revStep);
-
-
-    
-
-
-% tracker = 7;
-% tracker = squeeze(data_mar_dim_frame(tracker, 1:3, :)); %(nanmeanfor ii = step_TO_HS(1,:)    
-% for step_TO_HS = step_fr_ft_XYZ
-%     %match step when the STRN marker starts to move 
-%     (find(diff(markerXYZ(1,:))<=1))
+% %Draft
+% revLStep = mod(leftStep,2);
+% leftStep = leftStep(revLStep);
+% 
+% revRStep = ~mod(rightStep,2);
+% rightStep = rightStep(revRStep);
 
 end
