3a78fd09cc52823d3798930b0292b0f276da75c8
mfturra <60787493+mfturra@users.noreply.github.com>
2020-05-21 09:37:19 -0400

    Mods were made to helper function
    Plotting function was modified to include the plotting of the times when heel strike was detected

--- plotMar_vel_acc_jerk.m
+++ plotMar_vel_acc_jerk.m
@@ -2,30 +2,37 @@ function plotMar_vel_acc_jerk(head,chest,hip,LThigh,RThigh,LLeg,RLeg,LFoot,RFoot
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Function plots the x y z vel, acc, and jerk
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Velocity for markers in the x & y coordinate frames
+%% Velocity for markers in the x coordinate frames
 
 figure(trialNum)
-
-%head essentials
 
 %head marker coordinates
 subplot(3,2,1)
 plot(head.marVelx,'k')
 hold on
 head_ylim = ylim;
+%toe strike
 for ii = 1:length(rev_step_TO_HS)
     plot([rev_step_TO_HS(ii,1) rev_step_TO_HS(ii,1)],[head_ylim(1) head_ylim(2)],'b')
 end
+%heel strike
+for ii = 1:length(rev_step_TO_HS)
+    plot([rev_step_TO_HS(ii,2) rev_step_TO_HS(ii,2)],[head_ylim(1) head_ylim(2)],'g')
+end
 title('Head Velx')
-
 
 %chest marker coordinates
 subplot(3,2,3)
 plot(chest.marVelx,'k')
 hold on
 chest_ylim = ylim;
+%toe strike
 for ii = 1:length(rev_step_TO_HS)
     plot([rev_step_TO_HS(ii,1) rev_step_TO_HS(ii,1)],[chest_ylim(1) chest_ylim(2)],'b')
+end
+%heel strike
+for ii = 1:length(rev_step_TO_HS)
+    plot([rev_step_TO_HS(ii,2) rev_step_TO_HS(ii,2)],[chest_ylim(1) chest_ylim(2)],'g')
 end
 title('Chest Velx')
 
@@ -34,8 +41,13 @@ subplot(3,2,5)
 plot(hip.marVelx,'k')
 hold on
 hip_ylim = ylim;
+%toe strike
 for ii = 1:length(rev_step_TO_HS)
-    plot([rev_step_TO_HS(ii) rev_step_TO_HS(ii)],[hip_ylim(1) hip_ylim(2)],'b')
+    plot([rev_step_TO_HS(ii,1) rev_step_TO_HS(ii,1)],[hip_ylim(1) hip_ylim(2)],'b')
+end
+%heel strike
+for ii = 1:length(rev_step_TO_HS)
+    plot([rev_step_TO_HS(ii,2) rev_step_TO_HS(ii,2)],[hip_ylim(1) hip_ylim(2)],'g')
 end
 title('Hip Velx')
 
@@ -45,8 +57,13 @@ plot(LThigh.marVelx,'b')
 hold on
 plot(RThigh.marVelx,'r')
 thigh_ylim = ylim;
+%toe strike
 for ii = 1:length(rev_step_TO_HS)
-    plot([rev_step_TO_HS(ii) rev_step_TO_HS(ii)],[thigh_ylim(1) thigh_ylim(2)],'k')
+    plot([rev_step_TO_HS(ii,1) rev_step_TO_HS(ii,1)],[thigh_ylim(1) thigh_ylim(2)],'k')
+end
+%heel strike
+for ii = 1:length(rev_step_TO_HS)
+    plot([rev_step_TO_HS(ii,2) rev_step_TO_HS(ii,2)],[thigh_ylim(1) thigh_ylim(2)],'m')
 end
 title('LThigh & RThigh Velx')
 
@@ -56,8 +73,13 @@ plot(LLeg.marVelx,'b')
 hold on
 plot(RLeg.marVelx,'r')
 leg_ylim = ylim;
+%toe strike
 for ii = 1:length(rev_step_TO_HS)
-    plot([rev_step_TO_HS(ii) rev_step_TO_HS(ii)],[leg_ylim(1) leg_ylim(2)],'k')
+    plot([rev_step_TO_HS(ii,1) rev_step_TO_HS(ii,1)],[leg_ylim(1) leg_ylim(2)],'k')
+end
+%heel strike
+for ii = 1:length(rev_step_TO_HS)
+    plot([rev_step_TO_HS(ii,2) rev_step_TO_HS(ii,2)],[leg_ylim(1) leg_ylim(2)],'m')
 end
 title('LLeg & RLeg Velx')
 
@@ -68,7 +90,108 @@ hold on
 plot(RFoot.marVelx,'r')
 foot_ylim = ylim;
 for ii = 1:length(rev_step_TO_HS)
-    plot([rev_step_TO_HS(ii) rev_step_TO_HS(ii)],[foot_ylim(1) foot_ylim(2)],'k')
+    plot([rev_step_TO_HS(ii,1) rev_step_TO_HS(ii,1)],[foot_ylim(1) foot_ylim(2)],'k')
+end
+%heel strike
+for ii = 1:length(rev_step_TO_HS)
+    plot([rev_step_TO_HS(ii,2) rev_step_TO_HS(ii,2)],[foot_ylim(1) foot_ylim(2)],'m')
 end
 title('LFoot & RFoot Velx')
+
+%% Acceleration for markers in the x coordinate frames
+
+figure(trialNum+100)
+
+%head marker coordinates
+subplot(3,2,1)
+plot(head.marAccx,'k')
+hold on
+head_ylim = ylim;
+%toe strike
+for ii = 1:length(rev_step_TO_HS)
+    plot([rev_step_TO_HS(ii,1) rev_step_TO_HS(ii,1)],[head_ylim(1) head_ylim(2)],'b')
+end
+%heel strike
+for ii = 1:length(rev_step_TO_HS)
+    plot([rev_step_TO_HS(ii,2) rev_step_TO_HS(ii,2)],[head_ylim(1) head_ylim(2)],'g')
+end
+title('Head Accx')
+
+%chest marker coordinates
+subplot(3,2,3)
+plot(chest.marAccx,'k')
+hold on
+chest_ylim = ylim;
+%toe strike
+for ii = 1:length(rev_step_TO_HS)
+    plot([rev_step_TO_HS(ii,1) rev_step_TO_HS(ii,1)],[chest_ylim(1) chest_ylim(2)],'b')
+end
+%heel strike
+for ii = 1:length(rev_step_TO_HS)
+    plot([rev_step_TO_HS(ii,2) rev_step_TO_HS(ii,2)],[chest_ylim(1) chest_ylim(2)],'g')
+end
+title('Chest Accx')
+
+%hip marker coordinates
+subplot(3,2,5)
+plot(hip.marAccx,'k')
+hold on
+hip_ylim = ylim;
+%toe strike
+for ii = 1:length(rev_step_TO_HS)
+    plot([rev_step_TO_HS(ii,1) rev_step_TO_HS(ii,1)],[hip_ylim(1) hip_ylim(2)],'b')
+end
+%heel strike
+for ii = 1:length(rev_step_TO_HS)
+    plot([rev_step_TO_HS(ii,2) rev_step_TO_HS(ii,2)],[hip_ylim(1) hip_ylim(2)],'g')
+end
+title('Hip Accx')
+
+%LThigh & RThigh marker overlayed
+subplot(3,2,2)
+plot(LThigh.marAccx,'b')
+hold on
+plot(RThigh.marAccx,'r')
+thigh_ylim = ylim;
+%toe strike
+for ii = 1:length(rev_step_TO_HS)
+    plot([rev_step_TO_HS(ii,1) rev_step_TO_HS(ii,1)],[thigh_ylim(1) thigh_ylim(2)],'k')
+end
+%heel strike
+for ii = 1:length(rev_step_TO_HS)
+    plot([rev_step_TO_HS(ii,2) rev_step_TO_HS(ii,2)],[thigh_ylim(1) thigh_ylim(2)],'m')
+end
+title('LThigh & RThigh Accx')
+
+%LLeg & RLeg marker overlayed
+subplot(3,2,4)
+plot(LLeg.marAccx,'b')
+hold on
+plot(RLeg.marAccx,'r')
+leg_ylim = ylim;
+%toe strike
+for ii = 1:length(rev_step_TO_HS)
+    plot([rev_step_TO_HS(ii,1) rev_step_TO_HS(ii,1)],[leg_ylim(1) leg_ylim(2)],'k')
+end
+%heel strike
+for ii = 1:length(rev_step_TO_HS)
+    plot([rev_step_TO_HS(ii,2) rev_step_TO_HS(ii,2)],[leg_ylim(1) leg_ylim(2)],'m')
+end
+title('LLeg & RLeg Accx')
+
+%LFoot & RFoot marker overlayed
+subplot(3,2,6)
+plot(LFoot.marAccx,'b')
+hold on
+plot(RFoot.marAccx,'r')
+foot_ylim = ylim;
+for ii = 1:length(rev_step_TO_HS)
+    plot([rev_step_TO_HS(ii,1) rev_step_TO_HS(ii,1)],[foot_ylim(1) foot_ylim(2)],'k')
+end
+%heel strike
+for ii = 1:length(rev_step_TO_HS)
+    plot([rev_step_TO_HS(ii,2) rev_step_TO_HS(ii,2)],[foot_ylim(1) foot_ylim(2)],'m')
+end
+title('LFoot & RFoot Accx')
+
 end
